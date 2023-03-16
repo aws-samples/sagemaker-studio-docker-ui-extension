157553924685ae9fd28dbd8f890448504678f4ca
Sam Edwards <samdwar@amazon.com>
2023-03-16 18:03:32 +1100

    using os.system instead of subprocess

--- sagemaker_studio_docker_ui/host_creator.py
+++ sagemaker_studio_docker_ui/host_creator.py
@@ -1,13 +1,12 @@
-import subprocess
 import os
        
 def create_host(instance_type):
     is_bash_profile = os.path.isfile("/home/sagemaker-user/.bash_profile")
     is_bashrc = os.path.isfile("/home/sagemaker-user/.bashrc")
-    command = ["sdocker","create-host","--instance-type", instance_type]
-    bash_comm = []
+    command = f"sdocker create-host --instance-type {instance_type} &"
+    bash_comm = ""
     if is_bash_profile:
-        bash_comm = ["/bin/bash", "/home/sagemaker-user/.bash_profile", "&&"]
+        bash_comm = "/bin/bash /home/sagemaker-user/.bash_profile &&"
     elif is_bashrc:
-        bash_comm = ["/bin/bash", "/home/sagemaker-user/.bashrc", "&&"]
-    subprocess.Popen(bash_comm + command)
+        bash_comm = "/bin/bash /home/sagemaker-user/.bashrc &&"
+    os.system(bash_comm + command)
