b5500450d073c2f8f24a69b2031848063a073877
Sam Edwards <samdwar@amazon.com>
2023-03-18 17:20:32 +1100

    Update host_terminator.py

--- sagemaker_studio_docker_ui/host_terminator.py
+++ sagemaker_studio_docker_ui/host_terminator.py
@@ -1,9 +1,9 @@
 import os
        
-def terminate_host(instance_type):
+def terminate_host(instance_id):
     is_bash_profile = os.path.isfile("/home/sagemaker-user/.bash_profile")
     is_bashrc = os.path.isfile("/home/sagemaker-user/.bashrc")
-    command = f"sdocker terminate-host --instance-type {instance_type} &>> /home/sagemaker-user/.sagemaker_studio_docker_cli/ui_commands.log &"
+    command = f"sdocker terminate-host --instance-id {instance_id} &>> /home/sagemaker-user/.sagemaker_studio_docker_cli/ui_commands.log &"
     bash_comm = ""
     if is_bash_profile:
         bash_comm = "source ~/.bash_profile && "
