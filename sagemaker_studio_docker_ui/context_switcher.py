60773139f8c173b126ffb9075b672b345f628897
Sam Edwards <samdwar@amazon.com>
2023-03-29 10:15:15 +1100

    Fix issues (#4)
    * Fixed typos
    
    * improved logging
    
    * added logging to switcher

--- sagemaker_studio_docker_ui/context_switcher.py
+++ sagemaker_studio_docker_ui/context_switcher.py
@@ -1,4 +1,6 @@
 import subprocess
+import logging as log
        
 def switch_context(context_name):
+        log.info(f"Switching context to {context_name}")
         subprocess.Popen(["docker","context","use", context_name])
